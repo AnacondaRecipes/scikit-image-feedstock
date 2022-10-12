a5e7a67482434bda54c6443013b8493254872180
Kai Tietz <ktietz@anaconda.com>
2022-10-12 14:05:08 +0200

    fix typo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 
 for /r . %%f in (*.pyx) do (
   echo "%%f ..."
-  cython -3 -f %%f || cmd /k "exit /b 0
+  cython -3 -f %%f || cmd /k "exit /b 0"
 )
 rmdir /s /q skimage/viewer/tests 
 %PYTHON% -m pip install . --no-deps -vv
