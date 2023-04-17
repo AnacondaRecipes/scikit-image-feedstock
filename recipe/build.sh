06e9b8468e28cdaca87b5ac05cb5788646fc48ed
ArishaMays1 <1arishamays1@gmail.com>
2023-04-17 15:41:30 -0400

    increasing thread no. to 2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 set -ex
 if [[ ${target_platform} == linux-ppc64le ]]; then
-  export OPENBLAS_NUM_THREADS=1
+  export OPENBLAS_NUM_THREADS=2
 fi
 # clean any cython-generated .c/.cpp files (as used by "make clean")
 find . -name "*.pyx" -exec ./tools/rm_pyx_assoc_c_cpp.sh {} \;
