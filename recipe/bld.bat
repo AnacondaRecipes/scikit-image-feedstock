1148705496992d0d4237d2b9e904574ec8f0e5db
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2025-01-21 08:36:44 +0000

    scikit-image 0.25.0 (#22)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,5 +15,6 @@ set "LDFLAGS="
 REM don't add d1trimfile option because clang doesn't recognize it.
 set "SRC_DIR="
 
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+REM Adding --config-settings=compile-args="-j4" to make the windows build more stable for meson-python and cython compilation.
+%PYTHON% -m pip install . -vvv --no-deps --no-build-isolation --config-settings=compile-args="-j4"
 if %ERRORLEVEL% neq 0 exit 1
